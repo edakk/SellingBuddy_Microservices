9ebdd2d55f4814dd9e154d9eed40bb1633b7f5c6
edakk <kokeda123@gmail.com>
2024-12-08 23:35:17 +0300

    Add project files.

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Events/IntegrationEvent.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Events
+{
+    public class IntegrationEvent
+    {
+        [JsonProperty]
+        public Guid Id { get; private set; }
+
+        [JsonProperty]
+        public DateTime CreatedDate { get; private set; }
+
+       
+       
+        public IntegrationEvent()
+        {
+            Id= Guid.NewGuid();
+            CreatedDate = DateTime.Now;
+
+        }
+
+        [JsonConstructor]
+        public IntegrationEvent(DateTime createdDate , Guid id)
+        {
+            Id= id;
+            CreatedDate = createdDate;
+        }
+    }
+}
